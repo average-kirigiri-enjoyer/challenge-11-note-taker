84b87ac8473b9f037a1738bbed820c3f5a150774
ethan <hertlethan@gmail.com>
2023-09-26 17:24:34 -0400

    added code to add new notes to note list as per user input on the front end

--- routes/notes.js
+++ routes/notes.js
@@ -4,8 +4,10 @@ SCS Boot Camp Module 11 Weekly Challenge - Note Taker
 Created 2023/09/26
 Last Edited 2023/09/26
 */
-
-const notes = require('express').Router();
+const notes = require("express").Router();
+const path = require('path');
+const fs = require("fs");
+const uuid = require("../helpers/uuid.js");
 
 /*notes.get('/', (req, res) =>
 {
@@ -17,10 +19,31 @@ notes.post('/', (req, res) =>
 {
   if (req.body)
   {
+    //review_id: uuid()
+    
     console.log(`post notes successful`);
     noteData = req.body;
     console.log(noteData);
     res.json(`post notes successful`);
+
+    fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', (err, data) =>
+    {
+      if (err)
+      {
+        console.error(err);
+      }
+      else
+      {
+        let existingNotes = JSON.parse(data);
+        console.log(existingNotes);
+        existingNotes.push(noteData);
+
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(existingNotes, null, 4),
+          (writeErr) => writeErr
+          ? console.error(writeErr)
+          : console.info('successfully updated notes'));
+      }
+    });
   }
   else
   {
